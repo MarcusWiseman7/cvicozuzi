afb8edab3d3cc0505849fafb48976589f1ab021c
Marcus Wiseman <md.wiseman@hotmail.com>
2021-01-30 16:59:01 +0100

    Test cors preflight

--- server/index.js
+++ server/index.js
@@ -29,6 +29,7 @@ async function start() {
 
     // App middleware
     app.use(cors());
+    app.options('*', cors());
     app.use(cookieParser());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
